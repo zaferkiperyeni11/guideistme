d7fef170193e1dffeede976a3ba428d6cdae3c06
zaferkiperyeni11 <zaferkiperyeni@gmail.com>
2026-04-05 19:30:50 +0300

    Admin panel API URL guncellendi

--- src/admin/IstGuide.AdminPanel/src/services/api.ts
+++ src/admin/IstGuide.AdminPanel/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:5001/api/v1',
+  baseURL: 'http://istguideme.runasp.net/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
